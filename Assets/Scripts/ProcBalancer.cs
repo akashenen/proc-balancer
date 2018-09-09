caa1232f7f4681200f7add3802f4e5903dd183b7
akashenen <akashenen@gmail.com>
2018-09-09 18:44:52 -0300

    Proc Balancer + Test Scene

---
+++ Assets/Scripts/ProcBalancer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Utility class used to balance out procs so the final result is always the closest possible
+/// to the desired chance.
+/// </summary>
+public class ProcBalancer {
+    private float desiredRate;
+    private float currentChance;
+    private float balanceRatio = 0.1f;
+    private readonly int decimalPlaces = 5;
+
+    /// <summary>
+    /// Constructor for the ProcBalancer with default balance ratio (0.1)
+    /// </summary>
+    /// <param name="desiredRate">The desired occurrence rate for the proc</param>
+    public ProcBalancer(float desiredRate) {
+        this.desiredRate = desiredRate;
+        currentChance = desiredRate;
+    }
+
+    /// <summary>
+    /// Constructor for the ProcBalancer
+    /// </summary>
+    /// <param name="desiredRate">The desired occurrence rate for the proc</param>
+    /// <param name="balanceRatio">How much will the balancer affect the chance:
+    /// lower values are more subtle while higher ones are more noticeable. Default = 0.1</param>
+    public ProcBalancer(float desiredRate, float balanceRatio) {
+        this.desiredRate = desiredRate;
+        this.balanceRatio = balanceRatio;
+        currentChance = desiredRate;
+    }
+
+    /// <summary>
+    /// Rolls the proc according to the current chance
+    /// </summary>
+    /// <returns>Whether the proc was successful or not</returns>
+    public bool Proc() {
+        float rounding = Mathf.Pow(10f, decimalPlaces);
+        if (Random.value > currentChance) {
+            currentChance += balanceRatio * desiredRate;
+            currentChance = Mathf.Round(currentChance * rounding) / rounding;
+            return false;
+        } else {
+            currentChance -= balanceRatio * (1f - desiredRate);
+            currentChance = Mathf.Round(currentChance * rounding) / rounding;
+            return true;
+        }
+    }
+
+    public float GetCurrentChance() {
+        return currentChance;
+    }
+}
---
+++ Assets/Scripts/ProcTester.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProcTester : MonoBehaviour {
+
+    public RectTransform line;
+    public RectTransform column;
+    public RectTransform background;
+    public InputField chanceInput;
+    public InputField triesInput;
+    public InputField balanceRatioInput;
+
+    private float desiredChance;
+    private float balanceRatio;
+    private int tries;
+    private ProcBalancer balancer;
+    private Dictionary<float, int> procCount = new Dictionary<float, int>();
+    private bool[] resultsBalancer;
+    private bool[] resultsNormal;
+    private List<RectTransform> columns = new List<RectTransform>();
+    private float highestChance;
+    private float lowestChance;
+
+    private float h;
+    private float w;
+
+    private void Start() {
+        h = background.rect.height;
+        w = background.rect.width;
+    }
+
+    public void Balance() {
+        Init();
+        float sumBalancer = 0f;
+        int balancedProcs = 0;
+        int normalProcs = 0;
+        for (int i = 0; i < tries; i++) {
+            if (procCount.ContainsKey(balancer.GetCurrentChance()))
+                procCount[balancer.GetCurrentChance()]++;
+            else
+                procCount.Add(balancer.GetCurrentChance(), 1);
+            highestChance = Mathf.Max(highestChance, balancer.GetCurrentChance());
+            lowestChance = Mathf.Min(lowestChance, balancer.GetCurrentChance());
+            sumBalancer += balancer.GetCurrentChance();
+            resultsBalancer[i] = balancer.Proc();
+            resultsNormal[i] = desiredChance >= Random.value;
+            RectTransform newColumn = Instantiate(column);
+            columns.Add(newColumn);
+            newColumn.transform.SetParent(background);
+            newColumn.transform.SetAsFirstSibling();
+            newColumn.anchoredPosition = new Vector2((w / tries) * i, 0);
+            newColumn.sizeDelta = new Vector2(w / tries, balancer.GetCurrentChance() * h);
+            if (resultsBalancer[i])
+                balancedProcs++;
+            if (resultsNormal[i])
+                normalProcs++;
+        }
+
+        LogProcs(balancedProcs, normalProcs);
+    }
+
+    private void Init() {
+        desiredChance = float.Parse(chanceInput.text);
+        balanceRatio = float.Parse(balanceRatioInput.text);
+        tries = int.Parse(triesInput.text);
+        line.localPosition = new Vector2(0, (desiredChance * h) - h / 2);
+        line.gameObject.SetActive(true);
+        highestChance = lowestChance = desiredChance;
+        balancer = new ProcBalancer(desiredChance, balanceRatio);
+        resultsBalancer = new bool[tries];
+        resultsNormal = new bool[tries];
+        foreach (RectTransform item in columns) {
+            Destroy(item.gameObject);
+        }
+        columns.Clear();
+    }
+
+    private void LogProcs(int balancedProcs, int normalProcs) {
+        string log = "";
+        log += "Tested " + desiredChance * 100 + "% chance in " + tries + " tries.\n";
+        log += "Balanced results hits: " + balancedProcs + " - " + 1f * balancedProcs / tries * 100 + "%\n";
+        log += "Normal results hits: " + normalProcs + " - " + 1f * normalProcs / tries * 100 + "%\n";
+        log += "Highest chance: " + highestChance + "; Lowest Chance: " + lowestChance;
+        log += "Chance occurence:\n";
+        foreach (var item in procCount) {
+            log += item.Key * 100 + "% - " + item.Value + " times\n";
+        }
+        Debug.Log(log);
+    }
+}
